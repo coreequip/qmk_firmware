481b0e9103c2764e6deb16b16a5720069bc4dc1b
core.equip <coreequip@users.noreply.github.com>
2019-11-06 19:41:51 +0100

    Added DEL on FUN+BSPC and MYCM on FN+X

--- keyboards/xd60/keymaps/coreequip/keymap.c
+++ keyboards/xd60/keymaps/coreequip/keymap.c
@@ -7,10 +7,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                    KC_LSFT, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RCTL, KC_RCTL, KC_UP,
                    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_DEL, MO(1), KC_LEFT, KC_DOWN, KC_RGHT),
 
-	[1] = LAYOUT_all(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO,
+	[1] = LAYOUT_all(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_DEL,
                    _______, LALT(KC_F4), _______, RALT(KC_5), _______, _______, _______, RALT(KC_Y), _______, RALT(KC_P), KC_PSCR, BL_DEC, BL_INC, KC_APP,
                    _______, RALT(KC_Q), RALT(KC_S), _______, _______, RALT(S(KC_SCLN)), _______, _______, _______, _______, _______, _______, _______, _______,
-                   _______, _______, _______, _______, KC_CALC, _______, KC_VOLD, KC_VOLU, KC_MUTE, KC_MPRV, KC_MNXT, _______, _______, _______, KC_PGUP,
+                   _______, _______, _______, KC_MYCM, KC_CALC, _______, KC_VOLD, KC_VOLU, KC_MUTE, KC_MPRV, KC_MNXT, _______, _______, _______, KC_PGUP,
                    _______, _______, LM(2,MOD_LALT), KC_MPLY, KC_INS, _______, KC_HOME, KC_PGDN, KC_END),
 
 	[2] = LAYOUT_all(_______, KC_P1, KC_P2, KC_P3, KC_P4, KC_P5, KC_P6, KC_P7, KC_P8, KC_P9, KC_P0, KC_PMNS, KC_PPLS, _______, _______,
